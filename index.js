66ec20d1da6a1a6889fae5ae384363f37097c437
Alexis <amart78@outlook.com>
2022-02-26 14:15:13 -0600

    commit tree 6c27448fd0b90958e451e1153426e5d72ecafeca

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+
+
+//define an `employee` variable and assign it to an `Object` containing
+// `name` and `streetAddress` keys
+
+const employee = {
+    name: 'employeeName',
+    streetAddress: 'employeeStreetAddress',
+};
+
+// updateEmployeeWithKeyAndValue(employee, key, value)
+// 1) "before each" hook for "returns an employee with the original key value pairs and the new key value pair"
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key] : value}
+}
+
+// destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+// 2) updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+// deleteFromEmployeeByKey(employee, key)
+// 3) deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+// 4) does not modify the original employee (it is non-destructive)
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// destructivelyDeleteFromEmployeeByKey(employee, key)
+// 5) returns employee without the deleted key/value pair
+// 6) modifies the original employee
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
